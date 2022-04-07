4969f7635afce6ac240a246565e6cde9c6c78ca2
Jacob Chvatal <jake@isnt.online>
2022-04-07 14:51:59 -0400

    works with test article

--- src/hello/core.clj
+++ src/hello/core.clj
@@ -1,8 +1,27 @@
 (ns hello.core
   (:require
-    [clojure.string :as string])
+    [clojure.string :as string]
+    [tesser.core :as t]
+    [clojure.core.matrix :as m])
   (:gen-class))
+
 
 (defn -main
   [& args]
+
+  (let
+    [k 7
+     n 10000
+     zero (m/zero-matrix k k)
+     vecs (for [_ (range n)] (shuffle (range k)))
+     a (->> vecs
+            (map m/permutation-matrix)
+            (reduce m/add zero))
+     b (->> (t/map m/permutation-matrix)
+            (t/reduce m/add zero)
+            (t/tesser [vecs]))
+     matrices-equal (if (= a b) "Yes" "No")]
+    (println (str "\n\nAre a and b equal? " matrices-equal "\n"))
+    (m/pm a))
+
   (println (str "Hello from " (string/upper-case "clojure!!!"))))
