10e856e7a1a32cf610601be7d201a8bb58807b28
Jacob Chvatal <jake@isnt.online>
2022-04-07 16:04:22 -0400

    ignore cpcache, devshell

--- flake.nix
+++ flake.nix
@@ -9,31 +9,30 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
-  outputs = { self, nixpkgs, flake-utils, clj-nix }:
 
+  outputs = { self, nixpkgs, flake-utils, clj-nix }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
         cljpkgs = clj-nix.packages."${system}";
 
-          hello-clj = cljpkgs.mkCljBin {
-            projectSrc = ./.;
-            name = "me.lafuente/cljdemo";
-            main-ns = "hello.core";
-            jdkRunner = pkgs.jdk17_headless;
-          };
+        hello-clj = cljpkgs.mkCljBin {
+          projectSrc = ./.;
+          name = "me.lafuente/cljdemo";
+          main-ns = "hello.core";
+          jdkRunner = pkgs.jdk17_headless;
+        };
 
-          hello-jdk = cljpkgs.customJdk {
-            cljDrv = self.packages."${system}".hello-clj;
-            locales = "en,es";
-          };
+        hello-jdk = cljpkgs.customJdk {
+          cljDrv = self.packages."${system}".hello-clj;
+          locales = "en,es";
+        };
 
-          hello-graal = cljpkgs.mkGraalBin {
-            cljDrv = self.packages."${system}".hello-clj;
-          };
-      in
+        hello-graal = cljpkgs.mkGraalBin {
+          cljDrv = self.packages."${system}".hello-clj;
+        };
 
-      {
+      in {
         packages = {
           hello-clj = hello-clj;
           hello-jdk = hello-jdk;
@@ -41,6 +40,14 @@
         };
 
         defaultPackage = hello-clj;
-      });
 
+        devShell = with pkgs; mkShell {
+          name = "clojure-fft-nix";
+          buildInputs = with cljpkgs; [
+            clojure
+            leiningen
+            clojure-lsp
+          ];
+        };
+      });
 }
